817850c32c1d78af8f718937c6239584cae7e421
LevAlyabev <maryflay@mail.ru>
2023-03-20 01:10:51 +0300

    Module-2

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -4,4 +4,7 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.home),
+    path('countries-list', views._list),
+    path('languages-list', views.languages),
+    path('country/<str:country>', views.country_page)
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,11 +1,45 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
+import json
+
+with open("country-by-languages.json", "r") as file:
+    countries = json.load(file)
+
 
 def home(request):
-    result = """
-    <h1 style="color:#34495E;
-        font-size:50px">DjangoCountries</h1>
-    
-    <h2 style="color:#85929E;
-        font-size:30px;font-family:Arial">Приветствие</h2>
-    """
-    return HttpResponse(result)
+    return render(request, 'index.html')
+
+
+def _list(request):
+    context = {
+        "countries": countries
+    }
+    return render(request, 'countries_list.html', context)
+    # return HttpResponse(countries)
+
+
+def languages(request):
+    languages = []
+
+    for item in countries:
+        for i in item["languages"]:
+            if i not in languages:
+                languages.append(i)
+    languages = sorted(languages)
+
+    context = {
+        "languages": languages
+    }
+    return render(request, 'languages-list.html', context)
+    # return HttpResponse(countries)
+
+
+def country_page(request, country):
+    for item in countries:
+        if item['country'] == country:
+            context = {
+                'item': item
+            }
+            return render(request, 'country-page.html', context)
+
+    return HttpResponseNotFound(f"<h1>Такой страны нет ({country})</h1>")
